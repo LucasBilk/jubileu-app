885f56e89d55d0a7c938253f817f52af18a0c8cf
Lucas Bilk <l.bilkm@gmail.com>
2025-04-14 17:00:12 -0300

    key base

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module JubileuApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.secret_key_base = "SECRET_KEY_BASE"
   end
 end
