f546702abf7bdf2adc56801776037709b1e5da18
Lucas Bilk <l.bilkm@gmail.com>
2025-04-15 12:13:29 -0300

    allow any host

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module JubileuApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.secret_key_base = "SECRET_KEY_BASE"
+
+    config.hosts << /.*/
   end
 end
